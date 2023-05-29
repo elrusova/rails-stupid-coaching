c12bdf506a8c4e6c6c50179b8b79b31d3bb4b528
Elena Rusinova <elena.rusinova.spb@gmail.com>
2023-05-29 13:02:53 +0200

    update

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def ask
+    @question
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = coach_answer(@question)
+  end
+
+  def coach_answer(question)
+    if question.include?('?') == true
+      return 'Silly question, get dressed and go to work!'
+    elsif question == 'I am going to work right now!'
+      return ''
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+end
